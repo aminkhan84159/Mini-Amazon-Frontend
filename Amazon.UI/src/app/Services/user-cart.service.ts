7b602318dea06f13ae8586f77301cd66ead3b117
aminkhan84159 <162017503+aminkhan84159@users.noreply.github.com>
2025-08-07 19:40:05 +0530

    Update user-cart.service.ts

--- Amazon.UI/src/app/Services/user-cart.service.ts
+++ Amazon.UI/src/app/Services/user-cart.service.ts
@@ -9,7 +9,7 @@ import { IGetCartById } from '../Models/GetCartById.Model';
 })
 export class UserCartService {
 
-  mainApi = 'https://localhost:7175/api/';
+  mainApi = 'https://mini-amazon-backend-iu4n.onrender.com/';
   http = inject(HttpClient);
 
   public addToCart(cart: ICart) {
